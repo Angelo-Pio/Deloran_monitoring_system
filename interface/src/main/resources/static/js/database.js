2a2f8fe0e805f696cf7ed138bae271be4b79ca87
Angelo Pio <angelopompeo0@gmail.com>
2024-06-16 16:43:23 +0200

    looking nicer

--- interface/src/main/resources/static/js/database.js
+++ interface/src/main/resources/static/js/database.js
@@ -43,10 +43,11 @@ $(document).ready(function () {
             success: function (data) {
                 // Assuming data is an array of objects
                 sys_data = data;
-                handleChartCreationAndUpdate("CPU_Usage",data,"cpu_usage");
-                handleChartCreationAndUpdate("RAM_Usage",data,"ram_usage");
-                handleChartCreationAndUpdate("NET_Usage",data,"net");
-
+                if(data.length !== 0) {
+                    handleChartCreationAndUpdate("CPU_Usage", data, "cpu_usage");
+                    handleChartCreationAndUpdate("RAM_Usage", data, "ram_usage");
+                    handleChartCreationAndUpdate("NET_Usage", data, "net");
+                }
 
             },
             error: function () {
